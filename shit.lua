c7493eb8923a06b5a913542afe7d4ac07195dbed
RoksanaKregiel <76454142+RoksanaKregiel@users.noreply.github.com>
2022-08-29 18:41:00 +0200

    Add files via upload

---
+++ shit.lua
@@ -0,0 +1,107 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("stalinhack - arsenal (beta build)", "BloodTheme")
+--AIMBOT
+local Aimbot = Window:NewTab("Aimbot")
+local AimbotSection = Aimbot:NewSection("helps you with aim")
+
+--VISUALS
+local Visuals = Window:NewTab("Visuals")
+local VisualsSection = Visuals:NewSection("see retards through the wall")
+VisualsSection:NewButton("esp", "read above nig", function()
+
+end)
+
+--Misc
+
+local Misc = Window:NewTab("Misc")
+local MiscSection = Misc:NewSection("shit")
+
+
+
+
+ 
+AimbotSection:NewButton("aimbot", "hold x for it", function()
+local TeamCheck = true
+local WallCheck = true
+local Key = "X"
+local BodyPart = "Head"
+local FOV = 120
+local Inset = game:GetService("GuiService"):GetGuiInset()
+local SC = Vector2.new(workspace.CurrentCamera.ViewportSize.X / 2, workspace.CurrentCamera.ViewportSize.Y / 2)
+local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+if string.len(Key) == 1 then
+    Key = string.upper(Key)
+end
+function NotObstructing(Destination, Ignore)
+    local Origin = workspace.CurrentCamera.CFrame.Position
+    local CheckRay = Ray.new(Origin, Destination - Origin)
+    local Hit = workspace:FindPartOnRayWithIgnoreList(CheckRay, Ignore)
+    return Hit == nil
+end
+function ClosestHoe()
+    local MaxDist, Nearest = math.huge
+    for I,V in pairs(game:GetService("Players"):GetPlayers()) do
+        if V ~= game:GetService("Players").LocalPlayer and V.Character and V.Character:FindFirstChild("Humanoid") then
+            if WallCheck then
+                if TeamCheck then
+                    if V.Team ~= game:GetService("Players").LocalPlayer.Team then
+                        local Pos, Vis = workspace.CurrentCamera:WorldToScreenPoint(V.Character[BodyPart].Position)
+                        if Vis and NotObstructing(V.Character[BodyPart].Position, {game:GetService("Players").LocalPlayer.Character, V.Character}) then
+                            local Diff = math.sqrt((Pos.X - SC.X) ^ 2 + (Pos.Y + Inset.Y - SC.Y) ^ 2)
+                            if Diff < MaxDist and Diff < FOV then
+                                MaxDist = Diff
+                                Nearest = V
+                            end
+                        end
+                    end
+                else
+                    local Pos, Vis = workspace.CurrentCamera:WorldToScreenPoint(V.Character[BodyPart].Position)
+                    if Vis and NotObstructing(V.Character[BodyPart].Position, {game:GetService("Players").LocalPlayer.Character, V.Character}) then
+                        local Diff = math.sqrt((Pos.X - SC.X) ^ 2 + (Pos.Y + Inset.Y - SC.Y) ^ 2)
+                        if Diff < MaxDist and Diff < FOV then
+                            MaxDist = Diff
+                            Nearest = V
+                        end
+                    end
+                end
+            else
+                if TeamCheck then
+                    if V.Team ~= game:GetService("Players").LocalPlayer.Team then
+                        local Pos, Vis = workspace.CurrentCamera:WorldToScreenPoint(V.Character[BodyPart].Position)
+                        if Vis then
+                            local Diff = math.sqrt((Pos.X - SC.X) ^ 2 + (Pos.Y + Inset.Y - SC.Y) ^ 2)
+                            if Diff < MaxDist and Diff < FOV then
+                                MaxDist = Diff
+                                Nearest = V
+                            end
+                        end
+                    end
+                else
+                    local Pos, Vis = workspace.CurrentCamera:WorldToScreenPoint(V.Character[BodyPart].Position)
+                    if Vis then
+                        local Diff = math.sqrt((Pos.X - SC.X) ^ 2 + (Pos.Y + Inset.Y - SC.Y) ^ 2)
+                        if Diff < MaxDist and Diff < FOV then
+                            MaxDist = Diff
+                            Nearest = V
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return Nearest
+end
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode[Key]) then
+        local Hoe = ClosestHoe()
+        if Hoe and Hoe.Character and Hoe.Character:FindFirstChild(BodyPart) then
+            local Pos, Vis = workspace.CurrentCamera:WorldToScreenPoint(Hoe.Character[BodyPart].Position)
+            if Vis then
+                mousemoverel(Pos.X - Mouse.X, Pos.Y - Mouse.Y)
+            end
+        end
+    end
+end)
+end)
+
